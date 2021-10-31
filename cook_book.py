e1d43c0f40075ce6cb2897b5e4041b285e96c0a6
Pavel Rudenko <pogorus@gmail.com>
2021-10-31 17:31:47 +0300

    Logger added

--- cook_book.py
+++ cook_book.py
@@ -1,3 +1,16 @@
+import datetime
+
+def logger_decorator(path_to_log):
+    def logger(old_function):
+        def new_function(*args, **kwargs):
+            with open(path_to_log, 'a') as f:
+                f.write(f'Date - {datetime.datetime.now()}; Function name - {old_function.__name__}; Arguments - {args} {kwargs}; ')
+                result = old_function(*args, **kwargs)
+                f.write(f'Result - {result}\n')
+                return result
+        return new_function
+    return logger
+
 def get_ingredients(line):
     ingredients = {}
     ingredients['ingredient_name'] = line.split('|')[0].strip()
@@ -5,6 +18,7 @@ def get_ingredients(line):
     ingredients['measure'] = line.split('|')[2].strip()
     return ingredients
 
+@logger_decorator('log.txt')
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
@@ -14,7 +28,6 @@ def get_shop_list_by_dishes(dishes, person_count):
             else:
                 shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * person_count
     return shop_list
-
 
 cook_book = {}
 with open('recipes.txt') as file:
